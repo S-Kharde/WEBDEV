aee865b0c52feecfdcf535e423b4d943df02d514
S-Kharde <khardesahil207@gmail.com>
2026-09-15 11:38:05 +0530

    new added

---
+++ basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount +" Value");
+
+console. log(`Hello my name is ${name} and my repo count is ${repoCount} `)
+
+let gamename= 'sahil007'
+
+console.log(gamename.charAt(5))
+console.log(gamename.length)
+console.log(gamename.slice(2,3))
+console.log(gamename.slice(3))
+
+//to get the complete string we can use name.slice(0)
+// bascially not putting end value
+//using negative indexing if we want to comlete string we can use name.slice(-length)
